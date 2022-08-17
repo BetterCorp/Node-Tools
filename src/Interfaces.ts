e5e9d4b98ce0b2b686a31c2e0eeaddb3695366f6
Mitchell R <support@bettercorp.co.za>
2022-08-17 22:23:07 +0200

    Updated latest code from Alex

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -12,6 +12,13 @@ export interface SimpleStatu<T = any> {
   value?: T;
 }
 
-// from: https://stackoverflow.com/a/73394054/8083582  
+// from: https://stackoverflow.com/a/73394054/8083582
 export type ParamsFromString<T extends string> =
-  T extends `${string}{${infer U}}${string}` ? U : never;
+  // Check if this string includes a param, and infer the param name,
+  // as well as the string before and after
+  T extends `${infer Pre}{${infer Param}}${infer Post}`
+    ? // A param exists in the string, so return it as a union type that
+      // includes all other params in the string with this param removed.
+      Param | ParamsFromString<`${Pre}${Post}`>
+    : // No params exist in these string, so return never.
+      never;
